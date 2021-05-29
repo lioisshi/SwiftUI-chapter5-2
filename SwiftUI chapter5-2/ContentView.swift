aba0404838532ce1f87a7fcc3c77f7a991a10f5d
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-05-29 14:33:01 +0900

    交互にThank you とありがとうが出てくるよ

--- SwiftUI chapter5-2/ContentView.swift
+++ SwiftUI chapter5-2/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  SwiftUI chapter5-2
+//  SwiftUI-Section5
 //
 //  Created by Hiroaki Ishiga on 2021/05/29.
 //
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var msg = "Thank you!"
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            Button("Tap"){
+                if msg == "Thank you!" {
+                    msg = "ありがとう"
+                    
+                }
+                else {
+                    msg = "Thank you!"
+                }
+            }.font(.headline)
+        Text("\(msg)")
+            .font(.largeTitle)
             .padding()
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
