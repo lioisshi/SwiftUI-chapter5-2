ea99863c297e6a5d1d6136bafc261b97a502338f
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-05-29 14:19:41 +0900

    Initial Commit

---
+++ SwiftUI chapter5-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI chapter5-2
+//
+//  Created by Hiroaki Ishiga on 2021/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI chapter5-2/SwiftUI_chapter5_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_chapter5_2App.swift
+//  SwiftUI chapter5-2
+//
+//  Created by Hiroaki Ishiga on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_chapter5_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
